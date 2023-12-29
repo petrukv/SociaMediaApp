160128cd4ca06763ad4085fc3b4fd383b8af9a74
Vadym <petrukvadd@gmail.com>
2023-12-29 23:02:28 +0200

    sada

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+
+    def __str__(self):
+        return self.user.username
